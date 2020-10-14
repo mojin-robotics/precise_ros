ff06efdb2276cbb7bc7cd69914641e595062a6d5
deleh <lehmann.denis@gmail.com>
2020-10-14 16:08:57 +0200

    add template for TCP interface

---
+++ precise_driver/src/precise_tcp_interface.cpp
@@ -0,0 +1,25 @@
+#include <precise_driver/precise_tcp_interface.h>
+
+namespace precise_driver
+{
+    PreciseTCPInterface::PreciseTCPInterface(std::string &ip)
+    {
+    
+    }
+
+    void PreciseTCPInterface::connect()
+    {
+    
+    }
+
+    void PreciseTCPInterface::disconnect()
+    {
+    
+    }
+
+    std::string PreciseTCPInterface::send(std::string &data)
+    {
+        return ""
+    }
+
+} // namespace precise_driver
