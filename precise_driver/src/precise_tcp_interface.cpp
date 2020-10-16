5dd8ee76104605bfd66103037198b3279d61df06
deleh <lehmann.denis@gmail.com>
2020-10-16 14:28:00 +0200

    add simple TCP communication

--- precise_driver/src/precise_tcp_interface.cpp
+++ precise_driver/src/precise_tcp_interface.cpp
@@ -1,25 +1,81 @@
 #include <precise_driver/precise_tcp_interface.h>
 
+using namespace boost::asio;
+using ip::tcp;
+
 namespace precise_driver
 {
-    PreciseTCPInterface::PreciseTCPInterface(std::string &ip)
+  
+    PreciseTCPInterface::PreciseTCPInterface(const std::string &ip, const unsigned &port)
     {
-    
+        _connected = false;
+        _ip = ip;
+        _port = port;
+        _socket.reset(new boost::asio::ip::tcp::socket(_io_service));
     }
+
 
     void PreciseTCPInterface::connect()
     {
-    
+        if (!_connected)
+	{
+            _socket->connect(tcp::endpoint(boost::asio::ip::address::from_string(_ip), _port));
+	    _connected = true;
+        }
+        else
+	{
+            disconnect();
+            connect();
+        }
     }
+
 
     void PreciseTCPInterface::disconnect()
     {
-    
+      if (_connected)
+      {
+          _socket->close();
+          _connected = false;
+      }
     }
 
-    std::string PreciseTCPInterface::send(std::string &data)
+
+    std::string PreciseTCPInterface::send(const std::string &data)
     {
-        return ""
+        if (!_connected)
+	  connect();
+	      
+        boost::system::error_code error;
+      
+	// Send
+        boost::asio::write(*_socket.get(), boost::asio::buffer(data + "\r\n"), error);
+        if (error)
+	{
+	    throw boost::system::system_error(error);
+        }
+
+	// Receive
+	std::string result = "";
+
+	for (;;)
+	{
+	    char buf[128];
+        
+	    size_t len = _socket->read_some(boost::asio::buffer(buf), error);
+
+	    std::string data(buf, buf + len);
+	    result += data;
+	    
+	    if (error == boost::asio::error::eof) // Connection was closed clean
+	        break;
+	    else if (len < 128)	                  // Buffer was not filled completely
+	        break;
+	    else if (error)	                  // Some error occured
+	      throw boost::system::system_error(error);
+
+	}
+
+        return result;
     }
 
 } // namespace precise_driver
