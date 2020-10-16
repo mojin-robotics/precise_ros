42c4f75c96c587808c138a64bb1c92b4883f6177
deleh <lehmann.denis@gmail.com>
2020-10-16 14:28:00 +0200

    add simple TCP communication

--- precise_driver/include/precise_driver/precise_tcp_interface.h
+++ precise_driver/include/precise_driver/precise_tcp_interface.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <boost/array.hpp>
 #include <boost/asio.hpp>
 
 namespace precise_driver
@@ -8,14 +9,19 @@ namespace precise_driver
     class PreciseTCPInterface
     {
     public:
-        PreciseTCPInterface(std::string &ip);
+        PreciseTCPInterface(const std::string &ip, const unsigned &port);
 
         void connect();
         void disconnect();
-        std::string send(std::string &data);
+        std::string send(const std::string &data);
 
     private:
-      
+        bool _connected;
+        std::string _ip;
+        unsigned _port;
+        boost::asio::io_service _io_service;
+        boost::shared_ptr<boost::asio::ip::tcp::socket> _socket;
+  
     }; // class
   
 } // namespace precise_driver
