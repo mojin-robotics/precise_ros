62a87585211f1eeb0405f9dadb781b84f242f1a4
Benjamin Maidel <benjamin.maidel@mojin-robotics.de>
2020-10-20 14:15:04 +0200

    indentation fix

--- precise_driver/src/precise_tcp_interface.cpp
+++ precise_driver/src/precise_tcp_interface.cpp
@@ -5,7 +5,6 @@ using ip::tcp;
 
 namespace precise_driver
 {
-  
     PreciseTCPInterface::PreciseTCPInterface(const std::string &ip, const unsigned &port)
     {
         _connected = false;
@@ -14,66 +13,69 @@ namespace precise_driver
         _socket.reset(new boost::asio::ip::tcp::socket(_io_service));
     }
 
-
     void PreciseTCPInterface::connect()
     {
         if (!_connected)
-	{
-            _socket->connect(tcp::endpoint(boost::asio::ip::address::from_string(_ip), _port));
-	    _connected = true;
+        {
+            try{
+                _socket->connect(tcp::endpoint(boost::asio::ip::address::from_string(_ip), _port));
+                _connected = true;
+            }catch(boost::system::system_error &e)
+            {
+                _connected = false;
+                throw boost::system::system_error(e);
+            }
         }
         else
-	{
+        {
             disconnect();
             connect();
         }
     }
 
-
     void PreciseTCPInterface::disconnect()
     {
-      if (_connected)
-      {
-          _socket->close();
-          _connected = false;
-      }
+        if (_connected)
+        {
+            _socket->close();
+            _connected = false;
+        }
     }
 
-
-    std::string PreciseTCPInterface::send(const std::string &data)
+    Response PreciseTCPInterface::send(const std::string &data)
     {
+        std::lock_guard<std::mutex> guard(this->_comm_mutex);
         if (!_connected)
-	  connect();
-	      
+            connect();
+
         boost::system::error_code error;
-      
-	// Send
-        boost::asio::write(*_socket.get(), boost::asio::buffer(data + "\r\n"), error);
+
+        // Send
+        boost::asio::write(*_socket.get(), boost::asio::buffer(data + "\n"), error);
         if (error)
-	{
-	    throw boost::system::system_error(error);
+        {
+            throw boost::system::system_error(error);
         }
 
-	// Receive
-	std::string result = "";
+        // Receive
+        std::string result = "";
 
-	for (;;)
-	{
-	    char buf[128];
-        
-	    size_t len = _socket->read_some(boost::asio::buffer(buf), error);
+        for (;;)
+        {
+            char buf[128];
 
-	    std::string data(buf, buf + len);
-	    result += data;
-	    
-	    if (error == boost::asio::error::eof) // Connection was closed clean
-	        break;
-	    else if (len < 128)	                  // Buffer was not filled completely
-	        break;
-	    else if (error)	                  // Some error occured
-	      throw boost::system::system_error(error);
+            size_t len = _socket->read_some(boost::asio::buffer(buf), error);
 
-	}
+            std::string data(buf, buf + len);
+            result += data;
+
+            if (error == boost::asio::error::eof) // Connection was closed clean
+                break;
+            else if (len < 128) // Buffer was not filled completely
+                break;
+            else if (error) // Some error occured
+                throw boost::system::system_error(error);
+        }
 
         return result;
     }
