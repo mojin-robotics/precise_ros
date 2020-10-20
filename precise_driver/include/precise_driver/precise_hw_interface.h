a1141f469cb1099d3620b8ac137b50f962d49fbe
Benjamin Maidel <benjamin.maidel@mojin-robotics.de>
2020-10-20 14:16:40 +0200

    updated hardware interface

--- precise_driver/include/precise_driver/precise_hw_interface.h
+++ precise_driver/include/precise_driver/precise_hw_interface.h
@@ -2,7 +2,12 @@
 
 #include <realtime_tools/realtime_publisher.h>
 #include <ros_control_boilerplate/generic_hw_interface.h>
+#include <precise_driver/pflex_device.h>
+#include <precise_driver/precise_tcp_interface.h>
 #include <ros/ros.h>
+
+#include <mutex>
+#include <condition_variable>
 
 #include <std_srvs/Trigger.h>
 #include <std_srvs/SetBool.h>
@@ -14,6 +19,7 @@ namespace precise_driver
     {
     public:
         PreciseHWInterface(ros::NodeHandle &nh, urdf::Model *urdf_model = NULL);
+        ~PreciseHWInterface();
 
         virtual void init();
         virtual void read(ros::Duration &elapsed_time);
@@ -27,6 +33,12 @@ namespace precise_driver
         ros::ServiceServer _power_srv;
         ros::ServiceServer _attach_srv;
 
+        std::shared_ptr<PFlexDevice> _device;
+        Profile _profile;
+        int _profile_no;
+
+        std::mutex _mutex_init;
+        std::condition_variable _cond_init;
 
         bool init_cb(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res);
         bool teachmode_cb(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res);
--- precise_driver/src/precise_hw_interface.cpp
+++ precise_driver/src/precise_hw_interface.cpp
@@ -7,24 +7,47 @@ namespace precise_driver
         : ros_control_boilerplate::GenericHWInterface(nh, urdf_model)
     {
         ros::NodeHandle pnh(nh_, "hardware_interface");
-        //TODO: init ros stuff
+
+        std::string ip;
+        pnh.param<std::string>("ip_address", ip, ip);
+        int port;
+        pnh.param<int>("port", port, port);
+        pnh.param<int>("profile_no", _profile_no, _profile_no);
+        pnh.param<int>("speed", _profile.speed, _profile.speed);
+        pnh.param<int>("speed2", _profile.speed2, _profile.speed2);
+        pnh.param<int>("accel", _profile.accel, _profile.accel);
+        pnh.param<int>("decel", _profile.decel, _profile.decel);
+        pnh.param<double>("accel_ramp", _profile.accel_ramp, _profile.accel_ramp);
+        pnh.param<double>("decel_ramp", _profile.decel_ramp, _profile.decel_ramp);
+        pnh.param<int>("in_range", _profile.in_range, _profile.in_range);
+        pnh.param<int>("straight", _profile.straight, _profile.straight);
+
+        ROS_INFO_STREAM("ip: "<<ip);
+        ROS_INFO_STREAM("port: "<<port);
+
+        _device.reset(new PFlexDevice(std::make_shared<PreciseTCPInterface>(ip, port)));
 
         _init_srv = nh_.advertiseService("init", &PreciseHWInterface::init_cb, this);
         _teachmode_srv = nh_.advertiseService("teach_mode", &PreciseHWInterface::teachmode_cb, this);
-        _home_srv = nh_.advertiseService("teach_mode", &PreciseHWInterface::home_cb, this);
-        _power_srv = nh_.advertiseService("teach_mode", &PreciseHWInterface::power_cb, this);
+        _home_srv = nh_.advertiseService("home", &PreciseHWInterface::home_cb, this);
+        _power_srv = nh_.advertiseService("power", &PreciseHWInterface::power_cb, this);
         _attach_srv = nh_.advertiseService("attach", &PreciseHWInterface::attach_cb, this);
+    }
+
+    PreciseHWInterface::~PreciseHWInterface()
+    {
     }
 
     void PreciseHWInterface::init()
     {
+        //Wait for hardware to init
+        std::unique_lock<std::mutex> lock(_mutex_init);
+        ROS_INFO("Waiting for robot init");
+        _cond_init.wait(lock);
+        ROS_INFO("Init Done");
+
         GenericHWInterface::init();
-
-        //TODO: open connection to hardware
-        //TODO: fill current joint state values with values from hardware
-
-        std::vector<double> joints;
-        joints.assign(num_joints_, 0.0);
+        std::vector<double> joints = _device->getJointPositions();
 
         for(size_t i = 0; i < num_joints_; i++)
         {
@@ -35,9 +58,7 @@ namespace precise_driver
 
     void PreciseHWInterface::read(ros::Duration &elapsed_time)
     {
-        //TODO: Read current state from hardware
-        std::vector<double> joints;
-        joints.assign(num_joints_, 0.0);
+        std::vector<double> joints = _device->getJointPositions();
 
         for(size_t i = 0; i < num_joints_; i++)
         {
@@ -50,7 +71,9 @@ namespace precise_driver
         // Safety
         enforceLimits(elapsed_time);
 
-        //TODO: Write desired state to hardware
+        if(_device->operable())
+            _device->moveJointSpace(_profile_no, joint_position_command_);
+
     }
 
     void PreciseHWInterface::enforceLimits(ros::Duration &period)
@@ -60,26 +83,50 @@ namespace precise_driver
 
     bool PreciseHWInterface::init_cb(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
     {
+        if(_device->init(_profile_no, _profile) && _device->home())
+        {
+            res.success = true;
+            _cond_init.notify_one();
+        }
+        else
+            res.success = false;
         return true;
     }
 
     bool PreciseHWInterface::teachmode_cb(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res)
     {
+        if(_device->freeMode(req.data))
+            res.success = true;
+        else
+            res.success = false;
+
         return true;
     }
 
     bool PreciseHWInterface::home_cb(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)
     {
-        return true;
+        if(_device->home())
+            res.success = true;
+        else
+            res.success = false;
+
     }
 
     bool PreciseHWInterface::power_cb(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res)
     {
+        if(_device->setHp(req.data, 5))
+            res.success = true;
+        else
+            res.success = false;
         return true;
     }
 
     bool PreciseHWInterface::attach_cb(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res)
     {
+        if(_device->attach(req.data))
+            res.success = true;
+        else
+            res.success = false;
         return true;
     }
 
